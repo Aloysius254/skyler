9d6abb45b1cabc7b65cf4e40c33b5e141c19e63e
aloysius mworia <aloysiusmworia@gmail.com>
2026-09-10 12:49:59 +0300

    Add real Firebase config

--- script.js
+++ script.js
@@ -83,14 +83,6 @@ let database;
 
 // Initialize Firebase
 <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Gallery Functions - Shared gallery visible to everyone via Firebase
 let uploadedPhotos = [];
 let database;
